74efbc618a68cf42fbc41649454bb16674474df5
zfpx <zfpx@126.com>
2016-03-06 15:53:15 +0800

    设置请求和响应

---
+++ 7.http/5.静态文件2.js
@@ -0,0 +1,27 @@
+//导入核心模块http
+var http = require('http');
+/**
+ * 1. 能在特定的IP 特定端口上监听 客户端的请求
+ * 2. 当请求到来的时候能执行监听函数，并返回响应
+ *
+ * 创建一个服务器
+ * 指定监听函数 每当有客户端请求到来的时候执行的函数
+ * request 代表客户端的请求，可以从中获取请求过来的信息
+ * response 代表向客户端发的响应，可以通过它向客户端发响应
+ *
+ */
+var fs = require('fs');
+var mime = require('mime');
+var path = require('path');
+var server = http.createServer(function(request,response){
+    var url = request.url;
+    response.setHeader('Content-Type',mime.lookup(request.url)+';charset=utf-8');//设置响应头
+    fs.readFile('.'+url,'utf8',function(err,data){
+            response.write(data);
+            response.end();
+    })
+});
+//在8080端口上进行监听 ，主机名是localhost
+// 0 - 65535
+// ps -ef | grep node
+server.listen(8080,'localhost');
