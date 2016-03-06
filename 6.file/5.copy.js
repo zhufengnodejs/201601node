7665d17596affc54239384e6689d725ecd7b1bc6
zfpx <zfpx@126.com>
2016-03-06 10:58:02 +0800

    1.读取文件

---
+++ 6.file/5.copy.js
@@ -0,0 +1,10 @@
+/**
+ * 1. 把src里的文件内容，写入到target文件里
+ * 2.写入时要先清空再写入
+ */
+
+function copy(src,target){
+
+}
+
+copy('./name.txt','./name.bak.txt');
--- 6.file/test.js
+++ 6.file/test.js
@@ -1,8 +1,12 @@
-
+function  cb(err,data){
+    if(err){
+        console.error(err);
+    }else{
+        console.log(data);
+    }
+}
 try{
-    setTimeout(function(){
-        throw Error('error');
-    },1000);
+    setTimeout(cb,1000);
 }catch(e){
     console.error(e);
 }
