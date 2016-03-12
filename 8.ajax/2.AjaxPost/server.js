3ecd38f75474cf2841c56067b91cab60ab2bb1bc
zfpx <zfpx@126.com>
2016-03-12 11:54:18 +0800

    发送 post请求

--- 8.ajax/2.AjaxPost/server.js
+++ 8.ajax/2.AjaxPost/server.js
@@ -14,17 +14,21 @@ http.createServer(function(req,res){
     //路径名
     var pathname = urlObj.pathname;
     if(pathname == '/'){
+        //读取文件的内容
         fs.readFile('./index.html','utf8',function(err,data){
                 res.end(data);
         })
     }else if(pathname == '/reg'){
         var result='';
+        //当读到客户端提交过来的数据时会触发data事件，然后调用回调函数
         req.on('data',function(data){
             result +=data;
         })
         req.on('end',function(data){
+            //把查询字符串转成对象
             var obj = querystring.parse(result);
             console.log(obj);
+            //发送响应
             res.end('ok');
         })
     }
