348375664d1827fa3567d735906b4ba64d5cb627
zfpx <zfpx@126.com>
2016-03-12 17:37:41 +0800

    事件

--- 10.event/self.js
+++ 10.event/self.js
@@ -9,22 +9,21 @@ util.inherits(Girl,EventEmitter);
 var girl = new Girl();
 function Boy(name){
     this.name = name;
-    this.say = function(words){
-        console.log(this.name,words);
+    this.say = function(thing){
+        console.log(thing);
     }
 }
 var xiaoming = new Boy('小明');
 var xiaohua = new Boy('小花');
 //注册监听 事件 订阅
-var m = xiaoming.say.bind(xiaoming,'看上就买吧');
-girl.addListener('看',m);
+girl.addListener('看',xiaoming.say);
 //注册监听 事件 订阅
-girl.on('看',xiaohua.say.bind(xiaohua,'喜欢就多看看吧'));
+girl.on('看',xiaohua.say);
 //发射事件 发布
-girl.emit('看');
-girl.removeListener('看',m);
+girl.emit('看','钻石');
+girl.removeListener('看',xiaoming.say);
 //girl.removeAllListeners('看');
-girl.emit('看');
+girl.emit('看','项链');
 /*//如果发射多次事件，只会触发一次
 girl.once('饿了',function(){
     console.log('吃饭');
