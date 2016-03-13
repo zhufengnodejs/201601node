99e115e54025e2bb89b978719b6c5cfbe7030cae
zfpx <zfpx@126.com>
2016-03-13 11:17:29 +0800

    事件

--- 11.stream/4.copy.js
+++ 11.stream/4.copy.js
@@ -1,5 +1,11 @@
-function copy(src,target){
+function copy2(src,target){
+    var rs = fs.createReadStream(src);
+    var ws = fs.createWriteStream(target);
+    rs.on('data', function (data) {
+        ws.write(data);
+    });
 
+    rs.on('end', function (data) {
+        ws.end();
+    });
 }
-
-copy('./index.txt','./write.txt');
