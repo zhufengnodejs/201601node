8e2cd9f6eb5409ae7ec382ba9e2cbc018711be66
zfpx <zfpx@126.com>
2016-03-13 16:17:01 +0800

    服务器端代理

--- 13.express/1.express.js
+++ 13.express/1.express.js
@@ -8,10 +8,20 @@ var express = require('express');
 var app = express();
 //配置路由
 //当用户访问/的时候，会执行后面的回调函数
-app.get('/',function(req,res){
+app.get('/hello',function(req,res){
     //send它可以自动判断参数类型,自动转换响应信息
     //并且自动设置Content-Type
-    res.send('hello world');
+    res.end('get hello');
+});
+app.post('/hello',function(req,res){
+    //send它可以自动判断参数类型,自动转换响应信息
+    //并且自动设置Content-Type
+    res.send('post hello');
+});
+app.all('/hello',function(req,res){
+    //send它可以自动判断参数类型,自动转换响应信息
+    //并且自动设置Content-Type
+    res.send('all hello');
 });
 //启动服务器
 app.listen(3000);
