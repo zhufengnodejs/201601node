d3874dae3cb71cbce02cde6ed51db4edba6c0bd9
zfpx <zfpx@126.com>
2016-03-26 17:06:58 +0800

    路由配置

--- 21.angular/test.js
+++ 21.angular/test.js
@@ -1,9 +1,21 @@
-
-var xxx = 'xxx';
-function parent(){
-    child(xxx);
-}
-function child(n){
-    console.log(n);
-}
-parent();
+/**
+ * filter
+ * map
+ * forEach
+ * reduce
+ * reduceRight
+ * indexOf
+ * lastIndexOf
+ * sort
+ * every
+ * some
+ *
+ **/
+var arr = [1,2,3];
+var s = arr.filter(function(num){
+    return num >0;
+});
+var s2 = arr.map(function(num){
+    return num *10;
+});
+console.log(s2);
