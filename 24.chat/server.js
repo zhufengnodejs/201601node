eff31d11ebae57dff3f35ad155ab62cc02a69901
zfpx <zfpx@126.com>
2016-03-27 18:07:55 +0800

    进行聊天

--- 24.chat/server.js
+++ 24.chat/server.js
@@ -10,18 +10,37 @@ app.get('/',function(req,res){
 var server = require('http').createServer(app);
 //再创建socket.io服务器
 var io = require('socket.io')(server);
-var clients = [];
+var clients = {};
 //监听客户端的连接事件
 // socket代表与某个客户端的连接对象
 io.on('connection',function(socket){
-    //把当前的socket缓存起来
-  clients.push(socket);
+ var username;
+ socket.send({user:'系统',content:'请输入用户名'});
+
   //监听 客户端的消息
   socket.on('message',function(msg){
-      //把客户端发过来的消息广播给所有的客户端
-     clients.forEach(function(client){
-         client.send(msg);
-     })
+      var result = msg.match(/^@(.+)\s(.+)$/);
+      if(result){
+          var toUser = result[1];
+          var content = result[2];
+          if(clients[toUser]){//通过用户名把对应的socket取出来
+              clients[toUser].send({user:username,content:'[私聊]'+content});
+          }else{
+              socket.send({user:'系统',content:'你想私聊的人不在线'});
+          }
+      }else{
+          if(username){
+              //把客户端发过来的消息广播给所有的客户端
+              for(var s in clients){
+                  clients[s].send({user:username,content:msg});
+              }
+          }else{
+              username = msg;
+              //属性名是用户名，值为对应的socket对象
+              clients[username] = socket;
+              socket.send({user:'系统',content:'你的用户名已经修改为'+username});
+          }
+      }
   })
 });
 //监听端口
